829fc18431535b14d423977ce29f3fe87392e429
Manish Kumar Namdeo <mknamdeo@gmail.com>
2023-02-08 17:00:28 +0530

    use effect with send option

--- src/components/useEffect-basics.jsx
+++ src/components/useEffect-basics.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+
   const sayHello = () => {
     console.log('hello there');
   };
@@ -11,12 +13,19 @@ const UseEffectBasics = () => {
   useEffect(() => {
     console.log("This will be called only at the time of initialization,")
   }, [])
+  useEffect(() => {
+    console.log("This will be called whenever there is a change in secondValue")
+  }, [secondValue])
   return (
     <div>
       <h1>value : {value}</h1>
       <button className='btn' onClick={() => setValue(value + 1)}>
         click me
       </button>
+      <button className='btn' onClick={() => setSecondValue(secondValue + 1)}>
+        change 2nd value
+      </button>
+
     </div>
   );
 };
